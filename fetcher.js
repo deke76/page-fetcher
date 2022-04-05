7dabc8e6c3f3c9c5f00455218bd419ab32b2749c
Darcy <darcy.lauder@shaw.ca>
2022-04-05 12:12:07 -0600

    stretch exercises

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,46 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
+const isInvalid = require('is-invalid-path');
+require('process');
 
+const RL = readline.createInterface(process.stdin, process.stdout);
 const strURL = process.argv[2];
 const strFilePath = process.argv[3];
-console.log(strFilePath);
 
-request((strURL), (error, response, body) => {
-  fs.writeFile(strFilePath, body, err => {
-    if (err) {
+if (isInvalid(strFilePath.slice(2))) {
+  console.log(strFilePath, 'is an invalid file path');
+  process.exit(1);
+};
+
+request(strURL, (err, response, body) => {
+  if (err) {
       console.log(err);
-      return;
+      process.exit(1);
+  } 
+    
+  fs.readFile(strFilePath, (error) => {
+    if (!error) {
+      console.log(error);
+      RL.question('File exists, would you like to overwrite it? ', answer => {
+        if (answer.toUpperCase === 'Y') {
+          fs.writeFile(strFilePath, body, err => {
+            if (err) {
+              console.log(err);
+              process.exit(0);
+            }
+          });
+        } else process.exit(0);
+      });
+    } else {
+      console.log(error);
+      fs.writeFile(strFilePath, body, err => {
+        if (err) {
+          console.log(err);
+          process.exit(1);
+        }
+        process.exit(0);
+      });
     }
   });
 });
